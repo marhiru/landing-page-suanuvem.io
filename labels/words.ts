c4b62c91dbb4b671e1baa7146ed9ade5ddad3512
marhiru <marcosgamern@gmail.com>
2025-08-01 17:53:49 -0300

    feat: setup some words to test roation

---
+++ labels/words.ts
@@ -0,0 +1,16 @@
+interface IWordRotation {
+    word: string;
+    containerSize: number;
+}
+
+export const wordsRotation: IWordRotation[] = [
+    {
+        word: "operação",
+        containerSize: 200,
+    },
+    {
+        word: "infraestrutura",
+        containerSize: 295,
+    },
+
+]
