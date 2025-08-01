294e68e2887ae2760c20ba4467a19aef6b3dc416
marhiru <marcosgamern@gmail.com>
2025-08-01 18:46:01 -0300

    feat: creating a -styled input

---
+++ components/common/input/input.tsx
@@ -0,0 +1,10 @@
+import { JSX } from "react";
+import { IInputSharedProps } from "./input.types";
+
+export function Input(props: IInputSharedProps): JSX.Element {
+    return (
+        <div>
+            <Input />
+        </div>
+    )
+}
