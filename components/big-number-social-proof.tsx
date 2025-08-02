597600dc6c2a08560d833da7ef3019cc32066a6e
marhiru <marcosgamern@gmail.com>
2025-08-01 21:26:51 -0300

    fix: fixed stagering

--- components/big-number-social-proof.tsx
+++ components/big-number-social-proof.tsx
@@ -15,15 +15,28 @@ export default function BigNumberSocialProof() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentWord(wordsRotation[Math.floor(Math.random() * wordsRotation.length)]);
+      setCurrentWord(
+        wordsRotation[Math.floor(Math.random() * wordsRotation.length)]
+      );
     }, 3000);
     return () => clearInterval(interval);
   }, []);
 
   const textVariants = {
-    initial: { opacity: 0, y: 20 },
-    animate: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: -20 }
+    initial: {
+      opacity: 0,
+      y: 20,
+      filter: "blur(8px)",
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      filter: "blur(0px)",
+    },
+    exit: {
+      opacity: 0,
+      y: -10,
+    },
   };
 
   return (
@@ -31,35 +44,18 @@ export default function BigNumberSocialProof() {
       <div className="mx-auto px-6 text-center">
         <h2 className="text-5xl/tight tracking-tight items-center justify-center font-bold mb-8 relative">
           {/* <AnimateSharedLayout> */}
-          <motion.span layout className="inline-block">
+          <motion.span className="inline-block">
             Migre sua{" "}
           </motion.span>
           <motion.div
-            layout
+            // layout
             className="inline-flex items-center justify-center ml-2 overflow-clip"
-            transition={{
-              layout: {
-                type: "spring",
-                stiffness: 280,
-                damping: 18,
-                mass: 1
-              },
-              width: {
-                type: "spring",
-                stiffness: 280,
-                damping: 18,
-                mass: 1
-              }
-            }}
             animate={{
               width: currentWord.containerSize,
-              transition: { 
-                delay: 0.2, 
-                type: "spring",
-                stiffness: 280,
-                damping: 18,
-                mass: 1
-              }
+              transition: {
+                ease: [0.25, 0.1, 0.25, 1],
+                delay: 0.171
+              },
             }}
           >
             <AnimatePresence mode="wait">
@@ -69,32 +65,26 @@ export default function BigNumberSocialProof() {
                 initial="initial"
                 animate="animate"
                 exit="exit"
-                transition={{ 
-                  type: "spring",
-                  stiffness: 280,
-                  damping: 18,
-                  mass: 1
-                }}
+                // transition={{
+                //   type: "spring",
+                //   stiffness: 280,
+                //   damping: 18,
+                //   mass: 1,
+                // }}
                 className="inline-block"
-                layout
               >
                 {currentWord.word}
               </motion.span>
             </AnimatePresence>
           </motion.div>
-          {/* </Animatel> */}
           <br />
           não importa a
-          <span className="text-6xl text-primary  bg-clip-text ml-2 italic font-normal tracking-tight font-serif">
+          <span className="text-6xl text-primary bg-clip-text ml-2 italic font-normal tracking-tight font-serif">
             tecnologia
           </span>
         </h2>
 
         <div className="flex flex-col w-full items-center justify-center sm:flex-row gap-4">
-
-          { /*<Button size={"lg"} className="text-base h-12" variant="outline">
-            SECOND OPTION
-          </Button> */}
           <Input placeholder="Qual é seu email para contato?" />
         </div>
 
